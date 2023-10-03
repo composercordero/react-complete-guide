90a7e92c65c832fd0493bd58276ea1405fb1c859
composercordero <composercordero@gmail.com>
2023-10-02 23:00:33 -0500

    first commit

---
+++ 04-Basic-components/src/components/Card.jsx
@@ -0,0 +1,10 @@
+const Card = ({title, image, description}) => {
+  return (<>
+    <li className="concept">
+    <img src={image} alt={title} />
+    <h2>{title}</h2>
+    <p>{description}</p>
+  </li>
+  </>)
+}
+export default Card
