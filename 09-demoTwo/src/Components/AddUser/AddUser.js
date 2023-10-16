23a25431eb55ef9e3abfea1bf37f951e5a8cbf44
composercordero <composercordero@gmail.com>
2023-10-16 16:51:30 -0500

    useRef

--- 09-demoTwo/src/Components/AddUser/AddUser.js
+++ 09-demoTwo/src/Components/AddUser/AddUser.js
@@ -1,7 +1,7 @@
 import Button from '../Button/Button'
 import Card from '../Card/Card';
 import styles from './AddUser.module.css'
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import ErrorModal from '../ErrorModal/ErrorModal'
 
 const initialUserState = {
@@ -12,19 +12,28 @@ const initialUserState = {
 
 const AddUser = ({handleUserList}) => {
   
-  const [user, setUser] = useState(initialUserState);
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
   const [error, setError] = useState();
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    if (user.name.trim().length === 0 || user.age.trim().length === 0){
+    const user = {
+      'id': Math.random().toString(),
+      'name': nameInputRef.current.value,
+      'age':ageInputRef.current.value
+    }
+    const enteredName = nameInputRef.current.value;
+    const enteredAge = ageInputRef.current.value;
+    if (enteredName.trim().length === 0 || enteredAge.trim().length === 0){
       setError({
         title:'Invalid Input',
         content: 'Please enter a valid name and age.'
       });
       return;
     }
-    if (+user.age < 1){
+    if (+enteredAge < 1){
       setError({
         title:'Invalid Input',
         content: 'Please enter a valid age.'
@@ -32,16 +41,8 @@ const AddUser = ({handleUserList}) => {
       return;
     }
     handleUserList(user)
-    setUser(initialUserState)
-  };
-
-  const handleUserInput = (input, value) => {
-    setUser((prev)=>{ return {
-      ...prev,
-      'id': Math.random().toString(),
-      [input]:value,
-    }
-    })
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
   };
 
   const handleError = () => {
@@ -59,12 +60,12 @@ const AddUser = ({handleUserList}) => {
       <form onSubmit={handleSubmit}>
 
         <label htmlFor='name'>Name</label>
-        <input type='text' id='name' value={user.name}
-          onChange={(e) => handleUserInput('name', e.target.value)}/>
+        <input type='text' id='name'
+          ref = {nameInputRef}/>
 
         <label  htmlFor='age'>Age (years)</label>
-        <input type='number' id='age' value={user.age}
-          onChange={(e) => handleUserInput('age', e.target.value)}/>
+        <input type='number' id='age'
+          ref = {ageInputRef}/>
 
       <Button type={'submit'}>Add User</Button>
 
