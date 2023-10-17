0d5d8e86826c0d67fdd4eef24463cf37adb9ebfe
composercordero <composercordero@gmail.com>
2023-10-17 10:10:05 -0500

    set time out and cleanup function

--- 10-Effect-Reducer-Context/src/components/Login/Login.js
+++ 10-Effect-Reducer-Context/src/components/Login/Login.js
@@ -12,8 +12,16 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(()=> {
-    setFormIsValid(
-    enteredEmail.includes('@') && enteredPassword.trim().length > 6);
+    const identifier = setTimeout(() => {
+      console.log('checking for form validity')
+      setFormIsValid(
+      enteredEmail.includes('@') && enteredPassword.trim().length > 6);
+    }, 500);
+
+    return () => {
+      console.log('cleanup')
+      clearTimeout(identifier)
+    }; //cleanup function || This runs before every side effect function
   },[enteredEmail, enteredPassword])
 
   const emailChangeHandler = (event) => {
