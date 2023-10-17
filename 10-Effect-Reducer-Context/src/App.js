73f6c9d10a337ba6e0889495751744f887807c0f
composercordero <composercordero@gmail.com>
2023-10-17 15:14:10 -0500

    AuthContext Consumer Option

--- 10-Effect-Reducer-Context/src/App.js
+++ 10-Effect-Reducer-Context/src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context';
 
 function App() {
   
@@ -28,13 +29,19 @@ function App() {
     setIsLoggedIn(false);
   };
 
+// Technically we don't need the react fragment anymore thanks to the authContext.provider
+
   return (
     <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
+      <AuthContext.Provider value={{
+        isLoggedIn: isLoggedIn,
+      }}>
+        <MainHeader onLogout={logoutHandler} />
+        <main>
+          {!isLoggedIn && <Login onLogin={loginHandler} />}
+          {isLoggedIn && <Home onLogout={logoutHandler} />}
+        </main>
+      </AuthContext.Provider>
     </React.Fragment>
   );
 }
