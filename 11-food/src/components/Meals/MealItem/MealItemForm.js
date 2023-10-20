d8fae19ba73ea623175de238aaefa22983482a81
composercordero <composercordero@gmail.com>
2023-10-20 12:45:47 -0500

    meal item

---
+++ 11-food/src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,14 @@
+import classes from './MealItemForm.module.css';
+
+const MealFormItem = () => {
+    return (<>
+        <form>
+        <input>
+            
+        </input>
+        </form>
+    
+    </>);
+};
+
+export default MealFormItem;
---
+++ 11-food/src/components/Meals/Meals.js
@@ -0,0 +1,12 @@
+import classes from './Meals.module.css';
+import MealsSummary from './MealsSummary';
+import AvailableMeals from './AvailableMeals';
+
+const Meals = (props) => {
+    return(<>
+        <MealsSummary />
+        <AvailableMeals />
+    </>);
+};
+
+export default Meals;
